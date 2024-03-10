5f79a280e2fb6f7264120a4313d067b68bb534a3
srivalli <srivalli@lyrostech.com>
2024-03-10 12:56:00 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello world</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import { useState} from "react";
+
+export default function Form() {
+    const[username, setUsername] = useState([]);
+    const[password, setPassword] = useState([]);
+    const[login, setLogin] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(username === 'John' && password === 'pass1234'){
+            setLogin('granted');
+        }else{
+            setLogin('denied');
+        }
+    };
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+    <div className='main'>
+    <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) =>
+            setUsername(e.target.value)}
+            placeholder="Enter Name"
+          ></input>
+        </div>
+        <div>
+          <input
+            type="text"
+            value={password}
+            onChange={(e) =>
+            setPassword(e.target.value)}
+            placeholder="Password"
+          ></input>
+        </div>
+          <button>Submit</button>
+          </form>
+          {login === 'granted' && <p style={{color:'green'}}>Access Granted</p>} 
+          {login === 'denied' && <p style={{color:'red'}}>Access Denied</p>}       
+        </div>
+    );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './login';
 import reportWebVitals from './reportWebVitals';
+import './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
